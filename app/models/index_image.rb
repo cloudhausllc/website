97cd36714dae7d291eb4e9f9d68060dc6168f44d
Luke Gaddie <gaddie.3@osu.edu>
2016-07-31 21:48:49 -0400

    BUGFIX: Newly added IndexImages should have the currently logged in user set as the owner.

--- app/models/index_image.rb
+++ app/models/index_image.rb
@@ -2,6 +2,8 @@ class IndexImage < ActiveRecord::Base
   has_attached_file :image, styles: { full: "800x400>", thumb: "200x100>" }, default_url: "/images/missing_image.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
+  before_validation :set_owner
+
   belongs_to :user
 
   default_scope {
@@ -14,4 +16,12 @@ class IndexImage < ActiveRecord::Base
   scope :active_images, -> {
     where(active: true)
   }
+
+  private
+
+  def set_owner
+    if User.current_user
+      self.user_id = User.current_user[:id]
+    end
+  end
 end
